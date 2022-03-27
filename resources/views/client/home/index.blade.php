119fa238ee401d0e389b4fecb04da3751e83ca74
vuhuyenvu <89784656+vuhuyenvu@users.noreply.github.com>
2022-03-27 10:47:38 +0200

    update

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -10,7 +10,7 @@
        
             <div class="hero__item set-bg" data-setbg="{{asset('client-template/img/hero/banner.jpg')}}">
                                     <div class="hero__text">
-                                        <span>FRUIT FRESH</span>
+                                        <span>FRUIT FRESH home</span>
                                         <h2>Vegetable <br />100% Organic</h2>
                                         <p>Free Pickup and Delivery Available</p>
                                         <a href="#" class="primary-btn">SHOP NOW</a>
