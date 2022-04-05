8bce2433a7bac8387b11cbba29679276f3ea57c8
Nghi Huynh <huynhhuunghist2000@gmail.com>
2022-04-05 13:56:39 +0700

    NGHI THEM ADMIN

---
+++ resources/views/admin/home/bo.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.template.master')
+
+@section('content')
+<div class="breadcrumbs">
+    <div class="breadcrumbs-inner">
+        <div class="row m-0">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Quản Lý Bộ</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li><a href="#">Quản Lý</a></li>
+                            <li class="active">Bộ</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="animated fadeIn">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <strong class="card-title">Danh Sách Bộ</strong>
+                    </div>
+                    <div class="table-stats order-table ov-h">
+                        <table class="table ">
+                            <thead>
+                                <tr>
+                                    <th class="serial">#</th>
+                                    <th>Mã Bộ</th>
+                                    <th>Tên Bộ</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i=1; ?>
+                                @foreach($ds_bo as $bo)
+                                <tr>
+                                    <td class="serial"><?php echo $i++; ?>.</td>
+                                    <td> {{$bo->bo_ma}} </td>
+                                    <td>{{$bo->bo_ten}}</td>
+                                    <td></td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div> <!-- /.table-stats -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
